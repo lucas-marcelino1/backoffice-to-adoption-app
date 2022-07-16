10b2a69a5a536c4a5ffe6fe0bb295d6b3a029a4e
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2022-07-16 17:24:38 -0300

    some adjustes

--- app/controllers/api/v1/adoptions_controller.rb
+++ app/controllers/api/v1/adoptions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AdoptionsController < ActionController::API
   def index
-    adoptions = Adoption.all
+    adoptions = Adoption.joins(:animal).where('animals.adopted is FALSE')
     response = []
     adoptions.each do |adoption|
       response << {id: adoption.id, title: adoption.title, description: adoption.description,
@@ -12,7 +12,7 @@ class Api::V1::AdoptionsController < ActionController::API
 
   def show
     adoption = Adoption.find(params[:id])
-    response = {title: adoption.title, description: adoption.description, user_pet_care: adoption.user_pet_care.name,
+    response = {id: adoption.id, title: adoption.title, description: adoption.description, user_pet_care: adoption.user_pet_care.name,
                 city: adoption.animal.city, animal_type: adoption.animal.animal_type, age: adoption.animal.age, 
                 sex: adoption.animal.sex, animal_name: adoption.animal.name, animal_id: adoption.animal.id}                     
     render status: 200, json: response
